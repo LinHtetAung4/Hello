9451520506e8afc111383f95300ee41e15b6b1de
LinHtetAung4 <alinhtet134@gmail.com>
2022-09-09 17:22:10 +0630

    Updated design and Level.java

--- app/src/main/java/com/example/multiplechoicegame/Level.java
+++ app/src/main/java/com/example/multiplechoicegame/Level.java
@@ -2,6 +2,7 @@ package com.example.multiplechoicegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -14,63 +15,70 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Level extends AppCompatActivity implements View.OnClickListener{
-TextView leveltxt;
-TextView Timetxt;
-TextView txtquestionno;
-TextView txtscore,txtquestion;
-Button ans_A,ans_B,ans_C,ans_D,submit,next;
-CountDownTimer cdTimer;
+public class Level extends AppCompatActivity implements View.OnClickListener {
+    TextView leveltxt;
+    TextView Timetxt;
+    TextView txtquestionno;
+    TextView txtscore, txtquestion;
+    Button ans_A, ans_B, ans_C, ans_D, submit, next;
+    CountDownTimer cdTimer;
 
-int questionIndex=1;
-int num1=0;
-int num2=0;
-int opeNum=0;
-int score;
-String operator="+";
-int selectedAnswer = -1;
-int answer=0;
-String level;
-ArrayList<Integer> optionList=new ArrayList<Integer>();
+    int questionIndex = 1;
+    int num1 = 0;
+    int num2 = 0;
+    int opeNum = 0;
+    int score;
+    String operator = "+";
+    int selectedAnswer = -1;
+
+    int answer = 0;
+    String level;
+    ArrayList<Integer> optionList = new ArrayList<Integer>();
+    Button clickedButton, selectedButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
-        level= getIntent().getStringExtra("Level");
-        leveltxt =(TextView)findViewById(R.id.idlevel0);
+        level = getIntent().getStringExtra("Level");
+        leveltxt = (TextView) findViewById(R.id.idlevel0);
         leveltxt.setText("Level: " + level);
-        Timetxt =(TextView)findViewById(R.id.idtimer);
+        Timetxt = (TextView) findViewById(R.id.idtimer);
         ans_A = findViewById(R.id.ans_A);
         ans_B = findViewById(R.id.ans_B);
         ans_C = findViewById(R.id.ans_C);
         ans_D = findViewById(R.id.ans_D);
         submit = findViewById(R.id.submit);
-        txtquestion=findViewById(R.id.question);
-        txtquestionno=findViewById(R.id.idquestionno);
-        txtscore=findViewById(R.id.idscore);
-        next=findViewById(R.id.next);
+        txtquestion = findViewById(R.id.question);
+        txtquestionno = findViewById(R.id.idquestionno);
+        txtscore = findViewById(R.id.idscore);
+        next = findViewById(R.id.next);
 
         ans_A.setOnClickListener(this);
         ans_B.setOnClickListener(this);
         ans_C.setOnClickListener(this);
         ans_D.setOnClickListener(this);
 
+
         next.setVisibility(View.INVISIBLE);
+        if(!level.equals("0")){
+            Timer();
+        }
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(questionIndex>10){
-                    Toast.makeText(Level.this,"Congratulation",Toast.LENGTH_LONG).show();
-                }
-                else{
+                if (questionIndex > 10) {
+                    Toast.makeText(Level.this, "Congratulation", Toast.LENGTH_LONG).show();
+                } else {
                     setUpQuestion();
                 }
             }
         });
-        submit.setOnClickListener(new View.OnClickListener() {
 
+
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 checkAnswer();
@@ -78,151 +86,146 @@ ArrayList<Integer> optionList=new ArrayList<Integer>();
                 submit.setVisibility(View.INVISIBLE);
             }
         });
-        if(level.equals("0")){
+        if (level.equals("0")) {
             Timetxt.setVisibility(View.INVISIBLE);
         }
         setUpQuestion();
-
-
     }
-    public void Timer(String _level){
+
+    public void Timer() {
         long miliSec = 0;
-        if (_level.equals("1")){
-           miliSec = 21000;
+        if (level.equals("1")) {
+            miliSec = 21000;
+        } else if (level.equals("2")) {
+            miliSec = 11000;
         }
-        else if(_level.equals("2")){
-            miliSec=11000;
-        }
-        cdTimer=new CountDownTimer( miliSec,1000) {
+        cdTimer = new CountDownTimer(miliSec, 1000) {
             @Override
             public void onTick(long l) {
-                Timetxt.setText("Timer : 00:"+l/1000);
-
+                Timetxt.setText("Timer : 00:" + l / 1000);
             }
             @Override
             public void onFinish() {
-                if(questionIndex<10){
+                if (questionIndex < 10) {
                     questionIndex++;
                     setUpQuestion();
-                }
-                else{
-                    Toast.makeText(Level.this,"Congratulations",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Level.this, "Congratulations", Toast.LENGTH_LONG).show();
                 }
             }
         }.start();
-
     }
-    public void setUpQuestion(){
+
+    public void setUpQuestion() {
         submit.setVisibility(View.VISIBLE);
         next.setVisibility(View.INVISIBLE);
 
-        txtquestionno.setText("Question: "+questionIndex+"/10");
+        txtquestionno.setText("Question: " + questionIndex + "/10");
         if (level.equals("1")) {
-            Timer(level);
-        }
-        else if(level.equals("2")){
-            Timer(level);
+
+            cdTimer.start();
+        } else if (level.equals("2")) {
+           cdTimer.start();
         }
         clearCheckedButton();
-        txtscore.setText("Score: "+score);
-        num1=randomNumber(12,1);
-        num2=randomNumber(12,1);
-        opeNum=randomNumber(4,1);
-        switch (opeNum){
-            case 1: answer=num1+num2;
-            operator="+";
-            break;
-            case 2:if(num1>num2){
-                answer=num1-num2;
-                operator="-";
-            }
-            else if(num2>num1){
-                Toast.makeText(Level.this,"Num1 is greater than num2",Toast.LENGTH_LONG).show();
-                setUpQuestion();
-            }
-            break;
+        txtscore.setText("Score: " + score);
+        num1 = randomNumber(12, 1);
+        num2 = randomNumber(12, 1);
+        opeNum = randomNumber(4, 1);
+        switch (opeNum) {
+            case 1:
+                answer = num1 + num2;
+                operator = "+";
+                break;
+            case 2:
+                if (num1 > num2) {
+                    answer = num1 - num2;
+                    operator = "-";
+                } else if (num2 > num1) {
+                    Toast.makeText(Level.this, "Num1 is greater than num2", Toast.LENGTH_LONG).show();
+                    setUpQuestion();
+                }
+                break;
 
-            case 3:answer=num1*num2;
-            operator="*";
-            break;
+            case 3:
+                answer = num1 * num2;
+                operator = "*";
+                break;
 
-            case 4:answer=num1/num2;
-            operator="/";
-            break;
+            case 4:
+                answer = num1 / num2;
+                operator = "/";
+                break;
 
         }
-        txtquestion.setText(String.valueOf(num1)+operator+String.valueOf(num2)+ " Will be ");
+        txtquestion.setText(String.valueOf(num1) + operator + String.valueOf(num2) + " Will be ");
         setUpOptions();
 
     }
-    public int randomNumber(int max, int min){
+
+    public int randomNumber(int max, int min) {
         int range = max - min + 1;
-        int rand = (int)(Math.random() * range) + min;
+        int rand = (int) (Math.random() * range) + min;
         return rand;
     }
-    public void setUpOptions(){
-        optionList=new ArrayList<Integer>();
+
+    public void setUpOptions() {
+        optionList = new ArrayList<Integer>();
         optionList.add((answer));
-        for(int i=0;i<3;i++){
-            int radNum= randomNumber(12,1);
-            while (optionList.contains(radNum)){
-                radNum=randomNumber(12,1);
+        for (int i = 0; i < 3; i++) {
+            int radNum = randomNumber(12, 1);
+            while (optionList.contains(radNum)) {
+                radNum = randomNumber(12, 1);
             }
             optionList.add((radNum));
         }
-
-
         Collections.shuffle(optionList);
-
+        Toast.makeText(Level.this,"Answer "+answer,Toast.LENGTH_LONG).show();
         ans_A.setText(optionList.get(0).toString());
         ans_B.setText(optionList.get(1).toString());
         ans_C.setText(optionList.get(2).toString());
         ans_D.setText(optionList.get(3).toString());
-
-
     }
-    public void checkAnswer(){
-     if(selectedAnswer==-1){
-         Toast.makeText(Level.this, "Please Choose one",Toast.LENGTH_LONG).show();
 
-     }
-     else{
-         if(answer==(selectedAnswer) && questionIndex<=10){
-             score++;
-             txtscore.setText("Score: "+score);
-
-             MediaPlayer ring= MediaPlayer.create(Level.this,R.raw.correct);
-             ring.start();
-         }
-         else{
-             MediaPlayer ring= MediaPlayer.create(Level.this,R.raw.wrong);
-             ring.start();
-         }
-
-
-         if(!(level.equals("0"))){
-             cdTimer.cancel();
-             cdTimer=null;
-         }
-         questionIndex++;
-     }
+    public void checkAnswer() {
+        if (selectedAnswer == -1) {
+            Toast.makeText(Level.this, "Please Choose one", Toast.LENGTH_LONG).show();
+        } else {
+            if (answer == (selectedAnswer) && questionIndex <= 10) {
+                score++;
+                txtscore.setText("Score: " + score);
+                MediaPlayer ring = MediaPlayer.create(Level.this, R.raw.correct);
+                ring.start();
+            } else {
+                MediaPlayer ring = MediaPlayer.create(Level.this, R.raw.mixkitwrong);
+                ring.start();
+            }
+            if (!(level.equals("0"))) {
+                cdTimer.cancel();
+            }
+            questionIndex++;
+        }
     }
 
 
     @Override
     public void onClick(View view) {
-
-        Button clickedButton = (Button) view;
+        if(selectedButton!=null){
+            selectedButton.setBackgroundColor(Color.LTGRAY);
+        }
+        clickedButton = (Button) view;
+        selectedButton = clickedButton;
         selectedAnswer = Integer.parseInt(clickedButton.getText().toString());
         clickedButton.setBackgroundColor(Color.GREEN);
 
-
     }
-    public void clearCheckedButton(){
+
+    public void clearCheckedButton() {
         ans_A.setBackgroundColor(Color.LTGRAY);
         ans_B.setBackgroundColor(Color.LTGRAY);
         ans_C.setBackgroundColor(Color.LTGRAY);
         ans_D.setBackgroundColor(Color.LTGRAY);
     }
-
 }
+
+
--- app/src/main/java/com/example/multiplechoicegame/MainActivity.java
+++ app/src/main/java/com/example/multiplechoicegame/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button level0,level1,level2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
